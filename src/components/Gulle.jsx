fda8a2f6252015b230a1efaf674c32ffdb9fd3c0
Goose <207717976+Gus-gallery@users.noreply.github.com>
2025-12-27 19:54:18 +0100

    made silly js animation crazier

--- src/components/Gulle.jsx
+++ src/components/Gulle.jsx
@@ -4,18 +4,18 @@ const Gulle = () => {
     function handleMouseMove(e) {
         const { clientX, clientY } = e;
         const centerX = window.innerWidth / 2;
-        const centerY = window.innerHeight /2 ;
+        const centerY = window.innerHeight / 2;
 
         const deltaX = clientX - centerX;
         const deltaY = clientY - centerY;
 
-        const rotateX = (deltaY / centerY) * 10; 
-        const rotateY = (deltaX / centerX) * -10; 
+        const rotateX = (deltaY / centerY) * 20; 
+        const rotateY = (deltaX / centerX) * -20; 
 
         const gulleElement = document.getElementById('gulle');
         if (gulleElement) {
-            gulleElement.style.transform = `perspective(1000px) rotateX(${rotateX*2}deg) rotateY(${rotateY*2}deg)`;
-            gulleElement.style.transition = 'transform 0.1s';
+            gulleElement.style.transform = `perspective(1000px) rotateX(${rotateX*20}deg) rotateY(${rotateY*2.5}deg)`;
+            gulleElement.style.transition = 'transform 0.3s';
             gulleElement.style.opacity = `${-rotateY}px ${rotateX}px 20px rgba(0, 0, 0, 0.3)`;
         }
     }
@@ -28,8 +28,11 @@ const Gulle = () => {
             onMouseMove={handleMouseMove}
         >
             <div>
-                <div className="w-full p-12 shadow-orange-300 bg-gradient-to-r from-red-800 via-orange-600 to-blue-900 rounded-full shadow-lg flex items-center justify-center text-blue-200 font-bold text-2xl overflow-hidden">
+                <div className="w-full p-12 shadow-stone-300 bg-gradient-to-r from-stone-800 via-orange-800 to-sky-900 rounded-full shadow-lg flex items-center justify-center text-white font-bold text-2xl overflow-hidden">
                     Gulle's dope ass site!
+                </div>
+                <div className="w-full py-4 text-sky-800 font-bold">
+                    confused and wild animation is a good thing?
                 </div>
             </div>
         </section>
